7c03c56c94d833ccf029b5739e6016e39c486dcf
Daniel Gajewski <beeklzwork@gmail.com>
2020-03-27 19:50:00 +0100

    Structured code

--- sted.py
+++ sted.py
@@ -1,19 +1,8 @@
-"""STED: text beautifier.
-
-This script allows the user to format their text according to printing
-standarts. Supports autocapitalize, autospaces after commas and dots etc.
-
-This tool accepts different text-files as (.txt) or (.docx) available
-in OS Windows and MacOS, but you can test it even with other file extensions.
-
-This script requires Python3 to be installed. Functions module is located at `sted/functions.py`. 
-
-Contains the following functions:
-    * stedtxt - the main function of the script
-"""
-
-
-import functions
+# STED: text beautifier. Python3 required.
+# Functions module located at `sted/functions.py`
+import functions as f
+# `sted/arguments.py`
+import arguments
 
 
 def stedtxt(unformatted_text: str,
@@ -21,56 +10,53 @@ def stedtxt(unformatted_text: str,
             capitals=False,
             find_mistakes=False,
             stats=False,
-            path=None):
+            path=None
+            ):
     """
-    Main STED function
+    Allows to format a text according to printing standarts.
+    Supports autocapitalize, autospaces after punctuation marks
+    etc. Works with (.txt), (.docx), (.rtf), try other extensions
+    on your own risk.
 
     Args:
-        unformatted_text (str): path to your text file / printed actual text
-        spaces (bool): removes extra spaces in your text
-        capitals (bool): replaces lowercase to Uppercase if a new sentence
-        find_mistakes (bool): checks for mistakes in a text, prints a log
-        stats (bool): prints statistics of your text
-        path (str or None): path to formatted file 
-
-    Returns:
-        formatted_text(str), stats(str): prints formatted text, statistics to terminal.
-
+        unformatted_text (str): path to your text file / inline text
+        spaces (bool): remove extra spaces
+        capitals (bool): autocapitalize where needed (new sentence)
+        find_mistakes (bool): check for mistakes in the text, print log
+        stats (bool): print statistics (letters, sentences etc)
+        path (str or None): path to write the file 
     """
-    original_text = functions.read_text(unformatted_text)
-    formatted_text = original_text[:]
+    formatted_text = f.read_text(unformatted_text)
     mistakes = ""
     statistics = ""
     console_log = ""
 
-    # if [format_spaces] is flagged:
     if spaces:
-        formatted_text = functions.format_spaces(formatted_text)
+        formatted_text = f.format_spaces(formatted_text)
 
-    # if [capitals] is flagged:
     if capitals:
-        formatted_text = functions.to_uppercase(formatted_text)
+        formatted_text = f.to_uppercase(formatted_text)
 
-    # if [find_mistakes] is flagged:
     if find_mistakes:
-        mistakes += functions.find_mistakes(formatted_text)
+        mistakes += f.find_mistakes(formatted_text)
 
-    # if [stats] is flagged:
     if stats:
-        statistics += functions.get_stats(formatted_text)
+        statistics += f.get_stats(formatted_text)
 
-    # Writing a file if [text_out] is flagged:
+    # Write a file if [text_out] is flagged:
     if path is not None:
         console_log += "[*] Additional info:\n" if stats or find_mistakes else ""
-        return console_log + str(statistics) + "\n" + str(mistakes) + "\n" + functions.write_file(path, formatted_text)
-
-    # Returning results to terminal if not
+        return console_log + statistics + "\n" + mistakes + f.write_file(path, formatted_text)
+    # Return results to console if not
     else:
         console_log += "[*] Formatted text:\n" + formatted_text
         console_log += "\n[*] Additional info:\n" if stats or find_mistakes else ""
-        return console_log + str(statistics) + "\n" + str(mistakes) if len(statistics) or len(mistakes) > 0 else console_log
+
+        if len(statistics or mistakes) > 0:
+            return console_log + statistics + "\n" + mistakes
+        return console_log
 
 
 if __name__ == "__main__":
-    args = functions.args()
+    args = arguments.args()
     print(stedtxt(*args))
