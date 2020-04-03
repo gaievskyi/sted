ab59fc47cd0842ee6175b57b9e7f41895efe1b79
beeklz <beeklzwork@gmail.com>
2020-04-03 16:16:13 +0100

    Minimize arguments module

--- scripts/arguments.py
+++ scripts/arguments.py
@@ -4,50 +4,28 @@ import sys
 
 def args():
     parser = argparse.ArgumentParser(
-        description="Small Python text beautifier. Formats texts or text files.",
+        description="Python text beautifier. Formats texts or text files.",
         usage=f"python {sys.argv[0]} path/text [-h] [-s] [-c] [-m] [-t] [-p path]"
     )
     # Input file or text [required]
-    parser.add_argument("input",
-                        metavar="path/text",
-                        nargs=1,
-                        help="Path to source file/Text itself"
-                        )
+    parser.add_argument("input", metavar="path/text", nargs=1,
+                        help="Path to source file/Text itself")
     # Format spaces [optional]
-    parser.add_argument("-s", "--spaces",
-                        required=False,
-                        action="store_const",
-                        const=True,
-                        default=False,
-                        help="Formats spaces in the text. Removes exta spaces."
-                        )
+    parser.add_argument("-s", "--spaces", required=False, action="store_const", const=True,
+                        default=False, help="Formats spaces in the text. Removes exta spaces.")
     # Formats capitals [optional]
-    parser.add_argument("-c", "--capitals",
-                        required=False,
-                        default=False,
-                        action="store_const",
-                        const=True,
-                        help="Capitalizes letters where needed (new sentence)")
+    parser.add_argument("-c", "--capitals", required=False, default=False, action="store_const",
+                        const=True, help="Capitalizes letters where needed (new sentence)")
     # Find mistakes [optional]
-    parser.add_argument("-m", "--mistakes",
-                        required=False,
-                        default=False,
-                        action="store_const",
-                        const=True,
-                        help="Finds mistakes and prints info. Only Polish supported.")
+    parser.add_argument("-m", "--mistakes", required=False, default=False, action="store_const",
+                        const=True, help="Finds mistakes and prints info. Only Polish supported.")
     # Print statistics [optional]
-    parser.add_argument("-t", "--stats",
-                        required=False,
-                        default=False,
-                        action="store_const",
-                        const=True,
-                        help="Returns the statistics of the text.")
+    parser.add_argument("-t", "--stats", required=False, default=False,
+                        action="store_const", const=True, help="Returns the statistics of the text.")
     # Write to file [optional]
-    parser.add_argument("-p", "--path",
-                        required=False,
-                        metavar="path",
-                        default=None,
-                        help="Path to output file('.txt', '.rtf', or '.doc')")
+    parser.add_argument("-p", "--path", required=False, metavar="path",
+                        default=None, help="Path to output file('.txt', '.rtf', or '.doc')")
+
     args = parser.parse_args()
     inpath = args.input[0]
     spaces = args.spaces
