29f81c31279c089f08c4f05c6c373a19a2f7191e
beeklz <beeklzwork@gmail.com>
2020-09-16 23:57:36 +0300

    Constants added / separated

---
+++ common/constants.py
@@ -0,0 +1,7 @@
+READ_AS_TEXT = "[*] File was not found. Read as a text? (Y/n): "
+ENTER_AGAIN = "[*] Please, enter a path to your file again: "
+INVALID_ANSWER_TRY_AGAIN = "[*] Invalid answer. Please, try again (Y/n): "
+REWRITE = "[*] Are you sure you want to rewrite the file? (Y/n): "
+
+POSITIVE_ANSWERS = ("Yes", 'Y', "y", "yes")
+NEGATIVE_ANSWERS = ("No", 'N', "n", "no")
